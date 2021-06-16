f13d213d57430aa7029218ea27b0df875e602df6
Ethan Ligon <ligon@berkeley.edu>
2021-06-15 17:29:14 -0700

    Archive extra material; future development in upg_vs_uct.org.

--- problematic_pii.py
+++ problematic_pii.py
@@ -1,20 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-"""The following contain personally identifiable information"""
-
-# Still need to:
-# -Check other files in TUP-data for pii (still need to check all that were not .dta or .csv)
-# -Identify redundant files and add those to a list to be deleted
-# -Probably don’t need zip files or .df files (try to find code that produces the .df files)
-# -Can defer scrubbing until we’re confident we can build all the results
-
 TUP-data/TUP_full.dta: s7_11a_b, s7_12a_b, s7_13a_b, s7_21a_b, s7_22a_b, s7_23a_b, enu_m
 TUP-data/random_assignment.dta: rname, husband_name, father_name, enumerator, respname, fathername, contact
-    
 TUP-data/csv/TUP_endline.csv: respondent, pop_name, father, husband, hh_head, tel, oth_tel
 TUP-data/csv/HHroster.csv: s11
 TUP-data/csv/ssbusiness_survey.csv: address, enu, s19_6, s19_7name, s19_7tel, s19_7add
@@ -22,25 +7,21 @@ TUP-data/csv/TUP_SSudan_full.csv: loc, enu, s7_11a, s7_12a, s7_13a, s7_21a, s7_2
 TUP-data/csv/TUP_baseline.csv: s7_11a_b, s7_12a_b, s7_13a_b, s7_21a_b, s7_22a_b, s7_23a_b
 TUP-data/csv/TUP_full.csv: s7_11a_b, s7_12a_b, s7_13a_b, s7_21a_b, s7_22a_b, s7_23a_b, enu_m
 TUP-data/csv/TUP_merged2014.csv: loc, lat, lat_d, lat_p, lat_m, lon, lon_d, lon_p, lon_m, enu, s7_11a, s7_12a, s7_13a, s7_21a, s7_22a, s7_23a, s21_5, enu_m
-    
 TUP-data/data/Baseline/household roster.dta: rname, s11
 TUP-data/data/Baseline/income activities.dta: rname, s31
 TUP-data/data/Baseline/member education.dta: rname, s21
 TUP-data/data/Baseline/social network.dta: rname, no1, no2
 TUP-data/data/Baseline/Tup programme sudan single file.dta: rname, fname, hname, hhname, contact, loc, lat, lat_d, lat_p, lat_m, lon, lon_d, lon_p, lon_m, enu, s7_11a, s7_12a, s7_13a, s7_21a, s7_22a, s7_23a, s21_5 
 TUP-data/data/Baseline/TUP_baseline.dta: s7_11a_b, s7_12a_b, s7_13a_b, s7_21a_b, s7_22a_b, s7_23a_b
-
 TUP-data/Baseline/household roster.dta: rname, s11
 TUP-data/Baseline/income activities.dta: rname, s31
 TUP-data/Baseline/member education.dta: rname, s21
 TUP-data/Baseline/social network.dta: rname, no1, no2
 TUP-data/Baseline/Tup programme sudan single file.dta: rname, fname, hname, hhname, contact, loc, lat, lat_d, lat_p, lat_m, lon, lon_d, lon_p, lon_m, enu, s7_11a, s7_12a, s7_13a, s7_21a, s7_22a, s7_23a, s21_5 
 TUP-data/Baseline/TUP_baseline.dta: s7_11a_b, s7_12a_b, s7_13a_b, s7_21a_b, s7_22a_b, s7_23a_b
-    
 TUP-data/Midline/members_education.dta: s21
 TUP-data/Midline/ssbusiness_survey.dta: address, enu, s19_6, s19_7name, s19_7tel, s19_7add
 TUP-data/Midline/TUP_midline.dta: enu_m
-    
 TUP-data/Endline/Agric&livestock.dta: respondent, pop_name, father, husband, hh_head
 TUP-data/Endline/Agriculture_cleaned.csv: respondent, pop_name, father, husband, hh_head
 TUP-data/Endline/Agriculture_cleaned.dta: respondent, pop_name, father, husband, hh_head
@@ -49,13 +30,8 @@ TUP-data/Endline/sections_2_7.dta: respondent, pop_name, father, husband, hh_hea
 TUP-data/Endline/sections_8_17.csv: respondent, pop_name, father, husband, hh_head
 TUP-data/Endline/sections_8_17.dta: respondent, pop_name, father, husband, hh_head
 TUP-data/Endline/sections_19_24.dta: respondent, pop_name, father, husband, hh_head
-    
 TUP-data/Randomization/merged_census_baseline.dta: rname, husband_name, father_name, enumerator, respname, fathername, contact
-
 TUP-data/Mobile/remote_survey_Nov_Dec_2015.dta: introRes_name, Identificationname_father, final_qpre_ph_no, final_qAlt_ph_no
 TUP-data/Mobile/remote_survey_Nov2015_April2016.dta: introRes_name, Identificationname_father, final_qpre_ph_no, final_qAlt_ph_no 
 TUP-data/Mobile/remote_survey_Nov2015_Feb2016.dta: introRes_name, Identificationname_father, final_qpre_ph_no, final_qAlt_ph_no
 TUP-data/Mobile/TUP_remote_survey.dta: introRes_name, Identificationname_father, final_qpre_ph_no, final_qAlt_ph_no 
-    
-    
-
